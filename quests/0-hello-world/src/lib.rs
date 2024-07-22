2229c79612fdff535f751bc197df8e830df15517
Elliot Voris <elliot@stellar.org>
2024-07-22 12:22:10 -0500

    update quest 0 to follow example contracts useage of String

--- quests/0-hello-world/src/lib.rs
+++ quests/0-hello-world/src/lib.rs
@@ -10,13 +10,13 @@
 // From our Soroban SDK, we import the following macros:
 // - contract: marks a type as being the type contract functions attach to
 // - contractimpl: exports the public functions defined in the implementation
-// - symbol_short: macro to create short strings up to 9 characters (a-zA-Z0-9_)
 // - vec: creates a `Vec` with the given items
 // We also import the following types from the SDK:
 // - Env: provides access to the environment the contract is executing within
-// - Symbol: represents strings up to 32 characters long (a-zA-Z0-9_)
+// - String: a contiguous, growable array of `u8`s used to hold characters in
+//   the alphabet (a-zA-Z0-9_)
 // - Vec: a sequential and indexable growable collection
-use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec};
+use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
 
 // Defining a "unit-like" struct in the following manner allows us to easily
 // implement the `HelloContract` type (and any required traits) without the need
@@ -38,21 +38,19 @@ pub struct HelloContract;
 // `HelloContract` into one place.
 impl HelloContract {
     /// Our publicly visible function (method) is called `hello`. This function
-    /// will receive a `to` argument, and return a Vec made up of `Symbol`s made
+    /// will receive a `to` argument, and return a Vec made up of `String`s made
     /// from "Hello" and the supplied `to` value.
     ///
     /// # Arguments
     ///
     /// * `env` - the environment in which the contract is running (you don't
     ///   need to supply this yourself)
-    /// * `to` - who we are greeting (in this case a `Symbol`)
-    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
-        // We are creating and returning a `Vec` containing two `Symbol` items:
-        // ["Hello","friend"] (depends on what is supplied as the `to` argument)
-        // The use of `Symbol::short` here is done because our given string is
-        // fewer than 10 characters. Otherwise, we could use `Symbol::new` for
-        // strings up to 32 characters.
-        vec![&env, symbol_short!("Hello"), to]
+    /// * `to` - who we are greeting (in this case a `String`)
+    pub fn hello(env: Env, to: String) -> Vec<String> {
+        // We are creating and returning a `Vec` containing two `String` items:
+        // ["Hello","friend"] (actual return value depends on what is supplied
+        // as the `to` argument)
+        vec![&env, String::from_str(&env, "Hello"), to]
     }
 }
 
